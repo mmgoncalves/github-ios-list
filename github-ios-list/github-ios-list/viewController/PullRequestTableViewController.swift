96e4466939b99120a16f569ef3c11943878444e2
Mateus Marques <marques.m05@gmail.com>
2017-10-31 16:15:58 -0200

    finish to configure pullRequestTableViewController

--- github-ios-list/github-ios-list/viewController/PullRequestTableViewController.swift
+++ github-ios-list/github-ios-list/viewController/PullRequestTableViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import CoreData
 import Kingfisher
 
-class PullRequestTableViewController: UITableViewController, ServiceDelegate {
+class PullRequestTableViewController: UITableViewController, ServiceDelegate, NSFetchedResultsControllerDelegate {
 
     var managedObjectContext: NSManagedObjectContext!
     var repository: RepositoryEntity!
@@ -21,23 +21,54 @@ class PullRequestTableViewController: UITableViewController, ServiceDelegate {
         
         self.viewModel = PullRequestViewModel(context: self.managedObjectContext, repository: self.repository)
         self.viewModel.serviceDelegate = self
+        self.viewModel.fetchResultControllerDelegate = self
+        
+        self.viewModel.initializeFethResultController()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        guard let sections = viewModel.fetchResultController.sections else {
+            fatalError("FetchResultController is empty")
+        }
+        
+        return sections.count
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        guard let sections = viewModel.fetchResultController.sections else {
+            fatalError("FetchResultController is empty")
+        }
+        
+        return sections[section].numberOfObjects
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = self.tableView.dequeueReusableCell(withIdentifier: "PullRequestTableViewCell", for: indexPath) as? PullRequestTableViewCell else {
+            
+            return UITableViewCell()
+        }
+        
+        guard let pullRequest = viewModel.fetchResultController.object(at: indexPath) as? PullRequestEntity else {
+            fatalError("FetchResultCOntroller is empty")
+        }
+        
+        cell.authorLabel.text = pullRequest.owner?.name
+        cell.bodyLabel.text = pullRequest.body
+        cell.titleLabel.text = pullRequest.title
+        cell.dateLabel.text = pullRequest.createdAt?.description
+        
+        if let imageString = pullRequest.owner?.avatarUrl {
+            let url = URL(string: imageString)
+            cell.avatar.kf.setImage(with: url)
+        }
+        
+        return cell
     }
 
     func onFinish() {
